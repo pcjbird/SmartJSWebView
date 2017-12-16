288aea10fbdbc2b80263b49eac70d514a9b70ba7
pcjbird <pcjbird@PCJBIRD-MAC.local>
2017-12-16 21:47:31 +0800

    init project

---
+++ SmartJSWebView/SmartJSWebView/SmartJSWebView.h
@@ -0,0 +1,19 @@
+//
+//  SmartJSWebView.h
+//  SmartJSWebView
+//
+//  Created by pcjbird on 2017/12/16.
+//  Copyright © 2017年 Zero Status. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmartJSWebView.
+FOUNDATION_EXPORT double SmartJSWebViewVersionNumber;
+
+//! Project version string for SmartJSWebView.
+FOUNDATION_EXPORT const unsigned char SmartJSWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartJSWebView/PublicHeader.h>
+
+
